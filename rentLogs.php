e9063fa2992f138521cc20ca0c2eabeb2e107dbf
Joseph Antonios <jpantonios2@gmail.com>
2022-06-17 15:18:36 -0400

    Images

--- rentLogs.php
+++ rentLogs.php
@@ -14,6 +14,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Ez Umbrella Admin</title>
       <link rel="stylesheet" type="text/css" href="css/dashboardStyle.css?v=<?php echo time(); ?>">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
@@ -46,7 +47,7 @@
       <div class="container">
          <table>
             <tr>
-            <th></th>
+            <th>#</th>
             <th>Email</th>
             <th>First Name</th>
             <th>Last Name</th>
